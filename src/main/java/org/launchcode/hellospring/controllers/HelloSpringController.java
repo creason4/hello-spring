58fb5216a7a54b08152b2e4bc3bea97e53cf852d
Laura Creason <creason4@gmail.com>
2020-03-05 12:29:54 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -0,0 +1,43 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloSpringController {
+
+    //Response to /hello?name=LaunchCode
+    @RequestMapping(value = "hello" , method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String thegreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", thegreeting);
+        return "hello";
+    }
+
+    // Responds to /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-lists";
+    }
+}
